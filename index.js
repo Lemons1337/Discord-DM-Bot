68845c4fca3b633b6ed92a9b08ac5e814682b30b
Lemons <24715897+Lemons1337@users.noreply.github.com>
2018-10-24 19:36:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,25 @@ const client = new Discord.Client();
 
 const config = require('./config.json');
 const Bot = require('./bot.js');
+const chalk = require('chalk');
 const fs = require('fs');
 
 const tokens = fs.readFileSync('tokens.txt', 'utf-8').replace(/\r/gi, '').split('\n');
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 var i = 0;
-setInterval(() => {
-    if (i > tokens.length) return;
-    new Bot(client, tokens[i]).send();
-    i++;
-}, config.login_delay);
+rl.question(chalk.blue('Guild Invite to Mass DM: '), (invite) => {
+    rl.question(chalk.blue('Message to Send: '), (message) => {
+        setInterval(() => {
+            if (i >= tokens.length) return;
+            new Bot(client, tokens[i], invite, message).send();
+            i++;
+        }, config.login_delay);
+    });
+});
