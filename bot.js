6f36e8886365b05f96958d70914963177791acbe
Lemons <24715897+Lemons1337@users.noreply.github.com>
2018-10-24 19:35:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,48 +1,50 @@
 const config = require('./config.json');
 
 const chalk = require('chalk');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+const request = require('request');
 
 class Bot {
-    constructor(client, token) {
+    constructor(client, token, invite, message) {
         this.client = client;
         this.token = token;
+        this.invite = invite;
+        this.message = message;
     }
     send() {
         this.client.on('ready', () => {
-            rl.question(chalk.blue('Guild ID to Mass DM: '), (guild) => {
-                rl.question(chalk.blue('Message to Send: '), (message) => {
-                    var g = 0;
-                    try {
-                        this.client.guilds.get(guild).members.map(member => {
-                            if (member.id === this.client.user.id) {
-                                console.log(chalk.yellow(`Warning | You can't DM yourself silly!`));
-                            } else {
-                                setTimeout(() => {
-                                    member.send(message).then(g => {
-                                        console.log(chalk.green(`Success | Sent DM to ${member.user.tag}!`));
-                                    }).catch(err => {
-                                        console.log(chalk.yellow(`Warning | User ${member.user.tag} has Direct Messages Disabled!`));
-                                    });
-                                }, config.delay * g++);
-                            }
-                        });
-                    } catch(err) {
-                        console.log('Invalid Guild ID!');
-                    }
-                });
+            var g = 0;
+            request({
+                method: "POST",
+                url: `https://discordapp.com/api/v7/invite/${this.invite}`,
+                json: false,
+                headers: {
+                    authorization: this.token
+                },
+            }, (error, response, body) => {
+                var json = JSON.parse(body);
+                var guild = json.guild.id;
+                try {
+                    this.client.guilds.get(guild).members.map(member => {
+                        if (member.id === this.client.user.id) {
+                            console.log(chalk.yellow(`Warning | You can't DM yourself silly!`));
+                        } else {
+                            setTimeout(() => {
+                                member.send(this.message).then(g => {
+                                    console.log(chalk.green(`Success | Sent DM to ${member.user.tag}!`));
+                                }).catch(err => {
+                                    console.log(chalk.yellow(`Warning | User ${member.user.tag} has Direct Messages Disabled!`));
+                                });
+                            }, config.delay * g++);
+                        }
+                    });
+                } catch(err) {
+                    console.log('Invalid Guild ID!');
+                }
             });
         });
-        try {
-            this.client.login(this.token);
-        } catch(err) {
+        this.client.login(this.token).catch(err => {
             console.log(chalk.red(`Error: Invalid Token | ${this.token}`));
-        }
+        });
     }
 }
 
